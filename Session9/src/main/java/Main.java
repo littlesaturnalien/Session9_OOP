c77b9bba9658a096b54454aa62028310d14d37a8
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2024-09-17 18:26:54 -0600

    Menú finalizado

--- Session9/src/main/java/Main.java
+++ Session9/src/main/java/Main.java
@@ -1,32 +1,58 @@
 import DAO.StudentDAO;
 import Models.Student;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student std1 = new Student("Karen", 20, "kmryfonseca@uamv.edu.ni");
-        Student std2 = new Student("César", 19, "casilva@uamv.edu.ni");
-        Student std3 = new Student("Silvio", 19, "samora@uamv.edu.ni");
-
         StudentDAO students = new StudentDAO();
+        Scanner scanner = new Scanner(System.in);
 
-        if(students.save(std1)){
-            System.out.println("Estudiante almacenado correctamente");
-        } else {
-            System.out.println("Ocurrió un problema al almacenar al estudiante");
+        String menu = "1. Agregar estudiante\n" +
+                "2. Buscar estudiante\n" +
+                "3. Mostrar estudiantes\n" +
+                "4. Salir\n" +
+                " ---> ";
+
+        int option = 0;
+
+        while (option != 4) {
+            System.out.print(menu);
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Ingrese el nombre del estudiante: ");
+                    String name = scanner.next();
+                    System.out.println("Ingrese la edad del estudiante: ");
+                    int age = scanner.nextInt();
+                    System.out.println("Ingrese el email del estudiante: ");
+                    String email = scanner.next();
+                    Student student = new Student(name, age, email);
+                    if(students.save(student)){
+                        System.out.println("\nEstudiante almacenado correctamente\n");
+                    } else {
+                        System.out.println("\nOcurrió un problema al almacenar al estudiante\n");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese el nombre del estudiante a buscar: ");
+                    String nameToFind = scanner.next();
+                    students.findByName(nameToFind);
+                    System.out.println();
+                    break;
+
+                case 3:
+                    students.showStudents();
+                    System.out.println();
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+            }
         }
 
-        if(students.save(std2)){
-            System.out.println("Estudiante almacenado correctamente");
-        } else {
-            System.out.println("Ocurrió un problema al almacenar al estudiante");
-        }
-
-        if(students.save(std3)){
-            System.out.println("Estudiante almacenado correctamente");
-        } else {
-            System.out.println("Ocurrió un problema al almacenar al estudiante");
-        }
-
-        students.showStudents();
     }
 }
